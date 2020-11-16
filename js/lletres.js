c39dfdb29ea3cef88cdac08cb5311e702c6ea59b
Javi <bisistems@gmail.com>
2020-11-16 13:13:31 +0100

    Fase 1

---
+++ js/lletres.js
@@ -0,0 +1,10 @@
+let nom = ['j','o','s','e','j','a','v','i','e','r'];
+
+for(i=0; i < nom.length; i++){
+
+    let resultado;
+
+    document.getElementById("respueta").innerHTML += nom[i]+"<br>";
+    console.log(resultado = nom[i]);
+
+}
